268c06190048be20fb714b7d800e9453f5711177
ankushsood8 <57043759+ankushsood8@users.noreply.github.com>
2022-02-10 12:58:01 +0530

    Update firstpage.php

--- firstpage.php
+++ firstpage.php
@@ -21,13 +21,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="firstpagecontroller.js"></script> 
 </head>
-<body ng-app="myAppp" ng-init="x=2" >
-<div style="display: flex;align-items: center;justify-content: center;width: 100%;height:700px" ng-show="x>0">
+<body ng-app="myAppp" >
+<div style="display: flex;align-items: center;justify-content: center;width: 100%;height:700px"ng-controller='ctrl' ng-show="x">
     <a href="#!admin" style="text-decoration: none">
-    <button type="button" class="btn btn-primary" style="border-radius: 5px;margin-right: 15px;height: 10%">Admin</button>
+    <button type="button" class="btn btn-primary" style="border-radius: 5px;margin-right: 15px;height: 10%" ng-click="setvalue()">Admin</button>
     </a>
     <a href="#!user" style="text-decoration: none">
-    <button type="button" class="btn btn-secondary" style="border-radius: 5px;height: 10%">User</button>
+    <button type="button" class="btn btn-secondary" style="border-radius: 5px;height: 10%" ng-click="setvalue()">User</button>
 </a>
 </div>
 <div class="ng-view" >  </div>
